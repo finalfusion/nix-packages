b3963817e42d3b7762c22c94cd0bd384fc5243a2
Daniël de Kok <me@danieldk.eu>
2020-08-12 18:13:38 +0200

    finalfrontier, finalfusion-utils: warn about future removal

--- default.nix
+++ default.nix
@@ -8,11 +8,15 @@ in {
   modules = import ./modules;
   overlays = import ./overlays;
 
-  finalfrontier = pkgs.callPackage ./pkgs/finalfrontier {};
+  finalfrontier = builtins.trace
+    "finalfrontier is in nixpkgs since 2020-07-26. This attribute will become an alias nixpkgs 20.09 is released."
+    (pkgs.callPackage ./pkgs/finalfrontier {});
 
-  finalfusion-utils = pkgs.callPackage ./pkgs/finalfusion-utils {
-    withOpenblas = true;
-  };
+  finalfusion-utils = builtins.trace
+    "finalfusion-utils is in nixpkgs since 2020-08-05. This attribute will be an alias when nixpkgs 20.09 is released."
+    (pkgs.callPackage ./pkgs/finalfusion-utils {
+      withOpenblas = true;
+    });
 
   python3Packages = pkgs.recurseIntoAttrs(
     pkgs.python3Packages.callPackage ./pkgs/python-modules {}
