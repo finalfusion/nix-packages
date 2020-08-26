85880c9316b6fe718ecd799aad367c40d9f23fee
Daniël de Kok <me@danieldk.eu>
2020-08-26 08:22:45 +0200

    python3Packages.finalfusion: warn about future removal

--- pkgs/python-modules/default.nix
+++ pkgs/python-modules/default.nix
@@ -1,5 +1,7 @@
 { callPackage }:
 
 {
-  finalfusion = callPackage ./finalfusion {};
+  finalfusion = builtins.trace
+    "python3Packages.finalfusion is in nixpkgs since 2020-08-17. This attribute will be an alias when nixpkgs 20.09 is released."
+    callPackage ./finalfusion {};
 }
